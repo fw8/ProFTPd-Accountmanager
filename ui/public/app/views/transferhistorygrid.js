c9641b2a9fd6e9b4cf36a69beabc7fa633b9ded4
Florian Wolpert <wolpert@freinet.de>
2019-10-22 09:35:30 +0200

    add icons and color to command field of transfer history

--- ui/public/app/views/transferhistorygrid.js
+++ ui/public/app/views/transferhistorygrid.js
@@ -24,7 +24,29 @@ Ext.define('app.views.transferhistorygrid', {
       width: 150,
       dataIndex: 'transfer_date',
     },
-    { text: 'Kommando', dataIndex: 'command' },
+    {
+      xtype: 'actioncolumn',
+      text: 'Aktion',
+      //align: 'center',
+      width: 70,
+      dataIndex: 'command',
+      menuDisabled: true,
+      getClass: function (val, meta, rec) {
+        return {
+          'RETR': 'x-fa fa-upload',
+          'STOR': 'x-fa fa-download',
+          'DELE': 'x-fa fa-trash',
+        }[val];
+      },
+      renderer: function(val) {
+        color = {
+          'RETR': 'green',
+          'STOR': 'orange',
+          'DELE': 'red',
+        }[val];
+        return '<div style="float:right; font-size: 13px; line-height: 1em; color: '+color+';">' + val + '</div>' // TDOD: better put style into css file
+      },
+    },
     { text: 'Datei', dataIndex: 'filename', flex: 1 },
     { text: 'Größe', dataIndex: 'bytes', renderer: function(value, record){ return (value.fileSize(1)) }},
     { text: 'Protokol', dataIndex: 'protocol' },
